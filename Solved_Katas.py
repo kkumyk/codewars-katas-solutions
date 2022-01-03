eeb99e865d1e97f8551d81817d1d4c7395808698
Karina Kumykova <k.kumykova@gmail.com>
2022-01-03 13:30:49 +0000

    add max diff - easy

--- Solved_Katas.py
+++ Solved_Katas.py
@@ -1,2 +1,8 @@
-# PyCharm Test
-print("PyCharm Test - first commit.")
+def max_diff(lst):
+
+    if len(lst) != 0:
+        return max(lst) - min(lst)
+    else:
+        return 0
+
+max_diff([1, 2, 3, -4])
