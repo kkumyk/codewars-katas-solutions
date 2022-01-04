d6e313629221b55d887f587177085dbc33926a19
Karina Kumykova <k.kumykova@gmail.com>
2022-01-04 10:04:54 +0000

    add print to max diff - easy kata

--- Solved_Katas.py
+++ Solved_Katas.py
@@ -1,3 +1,8 @@
+# Unique Sum - 7Kyu
+def unique_sum(lst):
+    return sum(set(lst)) if lst else None
+print("The unique sum of the numbers in a given list is: ", unique_sum([2, 3, 3, 3]))
+
 # Max diff - easy - 7Kyu
 def max_diff(lst):
 
