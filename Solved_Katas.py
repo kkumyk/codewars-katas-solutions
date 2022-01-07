29340baa97eb3cc4677d7483ba4c7befb7eb74b4
Karina Kumykova <k.kumykova@gmail.com>
2022-01-07 10:23:07 +0000

    add an alternative solution to the repeatIt kate

--- Solved_Katas.py
+++ Solved_Katas.py
@@ -2,10 +2,23 @@
 
 
 # repeatIt - 8Kyu
-def repeat_it(string, n):
-    return string * n
 
-print(repeat_it('Hello', 3))
+# Create a function that takes a string and an integer (n).
+# The function should return a string that repeats the input string n number of times.
+# If anything other than a string is passed in you should return "Not a string"
+def repeat_it(string,n):
+    return string * n if type(string) == str else 'Not a string'
+
+print(repeat_it("*-*", 5))
+
+
+# Alternative solution using isinstance function which returns True if the specified object is of the specified type,
+# otherwise Falce:
+
+def repeat_it(string, n):
+    return string * n if isinstance(string, str) else 'Not a string'
+
+print(repeat_it("*^*", 5))
 
 #  Capitalization and Mutability - 8Kyu
 # fix the function below:
