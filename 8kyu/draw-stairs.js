6e415d36a28598515d4a18c3b7430413b899e13e
kkumykova <38696352+kkumykova@users.noreply.github.com>
2023-01-13 19:54:45 +0000

    add solution to draw stairs kata

--- 8kyu/draw-stairs.js
+++ 8kyu/draw-stairs.js
@@ -1,1 +1,23 @@
 
+function drawStairs(n) {
+  let result = [];
+  
+  for (let i = 0; i < n; i++) {
+    result[i] = `${' '.repeat(i)}I`;
+  }
+  
+  return result.join('\n');
+}
+
+
+// function drawStairs(n) {
+//     var count = 0;
+//     var result = '';
+  
+//     while(n > count) {
+//       result += ' '.repeat(count) + 'I';
+//       count++;
+//       if(n > count) result += '\n';
+//     }
+//     return result;
+// }
