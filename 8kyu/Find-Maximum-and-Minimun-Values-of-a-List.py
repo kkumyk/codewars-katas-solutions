ff8f0383452262fe4c27e4956316f1613fd9660b
kkumyk <k.kumykova@gmail.com>
2023-12-15 11:32:25 +0000

    Find Maximum and Minimum of a List solution

--- 8kyu/Find-Maximum-and-Minimun-Values-of-a-List.py
+++
@@ -1,6 +0,0 @@
-# Find Maximum and Minimum Values of a List - 8Kyu
-def minimum(arr):
-    return min(arr)
-
-def maximum(arr):
-    return max(arr)
---
+++ 8kyu/findMaxMinList.js
@@ -0,0 +1,14 @@
+
+/*Find Maximum and Minimum Values of a List*/
+
+let min = function(list){
+
+    return Math.min(...list);
+}
+
+let max = function(list){
+    
+    return Math.max(...list);
+}
+
+console.log(min([-52, 56, 30, 29, -54, 0, -110]))
