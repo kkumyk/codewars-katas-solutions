16fce63382bb2d25d95cd162c56b79cbcdef4a95
kkumyk <k.kumykova@gmail.com>
2023-12-15 10:15:20 +0000

    Well of Ideas solution

--- 8kyu/Well-of-Ideas-Easy-Version.py
+++
@@ -1,7 +0,0 @@
-def well(x):
-    if x.count('good') > 2:
-        return 'I smell a series!'
-    elif x.count('good') == 2 or x.count('good') == 1:
-        return 'Publish!'
-    else:
-        return 'Fail!'
