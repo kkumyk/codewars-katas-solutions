49dec13a981df58f5f035ddfe284cfdfc68ee53a
kkumyk <k.kumykova@gmail.com>
2023-12-18 11:20:42 +0000

    update file format

--- 8kyu/asciiTotal.js
+++ 8kyu/asciiTotal.js
@@ -1,10 +1,3 @@
-// # ASCII Total
-// def uni_total(s):
-//     sum = 0
-//     for l in s:
-//         sum += ord(l)
-//     return sum
-
 /*
 You'll be given a string, and have to return the sum of all characters as an int.
 The function should be able to handle all printable ASCII characters.
