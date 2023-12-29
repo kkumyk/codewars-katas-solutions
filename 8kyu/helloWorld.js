ec4802ee881d03aedddec77823a41f5b324c2b2a
kkumyk <k.kumykova@gmail.com>
2023-12-29 11:44:08 +0000

    Hello World solution

---
+++ 8kyu/helloWorld.js
@@ -0,0 +1,11 @@
+/*
+Make a simple function called greet that returns the most-famous "hello world!".
+*/
+let message = "hello world!";
+let person = "Karina";
+
+function greet() {
+    if (person.length > 0) {
+        return message;
+    }
+}
