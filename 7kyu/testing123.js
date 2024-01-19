8f5870ecc8e6ed6ae48debdfb1ee1c1ec0063f44
kkumyk <k.kumykova@gmail.com>
2024-01-19 10:26:41 +0000

    Add second solution for Testing 1 2 3

--- 7kyu/testing123.js
+++ 7kyu/testing123.js
@@ -2,14 +2,20 @@
 Write a function which takes a list of strings and returns each line prepended by the correct number.
 */
 
+// Solution 1
 let number = function (array) {
     let result = [];
 
     for (let i = 1; i <= array.length; i++) {
-        result.push(String(i)  + ": " + array[i-1])
+        result.push(String(i) + ": " + array[i - 1])
     }
 
     return result;
 }
 
+// Solution 2
+let number2Solution = (array) => array.map((v, i) => `${i + 1}: ${v}`)
+
 console.log(number(["a", "b", "c"])) // ["1: a", "2: b", "3: c"]
+
+console.log(number2Solution(["a", "b", "c"])) // ["1: a", "2: b", "3: c"]
