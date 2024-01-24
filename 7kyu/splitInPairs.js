da27147ad7d6e2a015be4e0187fe4ff0b43bd42e
kkumyk <k.kumykova@gmail.com>
2024-01-24 18:34:36 +0000

    change var to let

--- 7kyu/splitInPairs.js
+++ 7kyu/splitInPairs.js
@@ -2,7 +2,7 @@
 The aim of this kata is to split a given string into different strings of equal size (note size of strings is passed to the method).
 */
 
-var splitInParts = function (s, partLength) {
+let splitInParts = function (s, partLength) {
 
     let result = [];
 
