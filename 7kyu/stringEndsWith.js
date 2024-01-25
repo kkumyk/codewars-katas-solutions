b3200033a0e70ce6b5ef9525dcbcecdba37f362f
kkumyk <k.kumykova@gmail.com>
2024-01-25 11:33:21 +0000

    String ends with solution

---
+++ 7kyu/stringEndsWith.js
@@ -0,0 +1,9 @@
+/*
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+*/
+
+// The endsWith() method of String values determines whether a string ends with the characters of this string, returning true or false as appropriate.
+
+function solution2(str, ending) {
+    return str.endsWith(ending);
+}
