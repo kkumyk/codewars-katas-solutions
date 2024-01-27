bbc1f31e709f7b6b171d3edd83450ff06ef3b7af
kkumyk <k.kumykova@gmail.com>
2024-01-27 15:16:20 +0000

    Get the middle character solution

---
+++ 7kyu/getTheMiddleCharacter.js
@@ -0,0 +1,12 @@
+
+
+function getMiddle(s) {
+
+    let len = s.length;
+    
+    return (len % 2 === 0) ? s[(len / 2) - 1] + s[len / 2] : s[Math.floor(len / 2)];
+};
+
+console.log(getMiddle("todo")) // od
+
+
