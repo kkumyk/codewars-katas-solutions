beccc750ebcb217256f1ce06921b61075aeb0c2a
kkumyk <k.kumykova@gmail.com>
2024-01-28 14:54:05 +0000

    Zalgo text reader solution

---
+++ 7kyu/zalgoTextReader.js
@@ -0,0 +1,10 @@
+
+
+//  \w represents a word character
+
+function readZalgo(str) {
+    return s.replace( /[^\w.,!? ]/g, "" ) ;
+}
+
+
+console.log(readZalgo("H̗̪͇͓̙͎̣̄ͬa͚̯̦͉̖̥v͆ͩ̃͆̓̐ͥe̟͎͖͕͍̎ ̰͚̩̟͕̰͊̽̍ͯ̌͊ā̖̪͉͍̥͙̿ͩ̃ͅ ̬̥͎͑̿ͧg̰̳̺͔̦͉ͫ̀̐̓̐r̝̫̱̘̰͐͋ͯͭͭͭ͆e͙͕̖̗͙̰͌ͭä͓͚̝͓́̌͑ͪ͊ṱͥ ̱ͣd͎͔͎͇̫̪̘̃͐̇à͕̮̈͋ͪy̼̳̱ͮ!̳̥̰̭͇̔ͮ̽̓")) // Have a great day!
