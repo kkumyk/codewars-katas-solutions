37378936cd8d37c9ffd7927c5eb56111c3d211b7
kkumyk <k.kumykova@gmail.com>
2024-01-30 18:16:35 +0000

    Number of people in the bus solution

--- 7kyu/numberOfPeopleInTheBus.py
+++ 7kyu/numberOfPeopleInTheBus.py
@@ -1,20 +1,17 @@
-
 # - input: an array of integer pairs
 # - each pair represent the number of people that get on and off the bus
-# - return the number of people who are still on the bus after the last bus 
-
-# function busStop(arr) {
-
-#     let totalIn = 0;
-#     let totalOut = 0;
-
-#     arr.forEach(element => {
-#         totalIn += element[0];
-#         totalOut += element[1];
-#     });
-
-#     return totalIn - totalOut;
-# }
+# - return the number of people who are still on the bus after the last bus
 
 
-# busStop([[3,0],[9,1],[4,10],[12,2],[6,1],[7,10]])) // 17
+def number(bus_stops):
+    totalIn = 0
+    totalOut = 0
+
+    for stop in bus_stops:
+        totalIn += stop[0]
+        totalOut += stop[1]
+
+    return totalIn - totalOut
+
+
+print(number([[3, 0], [9, 1], [4, 10], [12, 2], [6, 1], [7, 10]]))
