e7d2da7b6851824084a1707cc361039b8be58a9b
kkumyk <k.kumykova@gmail.com>
2024-02-07 13:30:25 +0000

    Merge strings alternately solution

---
+++ 7kyu/merge_strings_alternately.py
@@ -0,0 +1,14 @@
+def mergeAlternately(word1, word2):
+
+    result = []
+    i = 0
+    while i < len(word1) or i < len(word2):
+        if i < len(word1):
+            result.append(word1[i])
+        if i < len(word2):
+            result.append(word2[i])
+        i += 1
+    return "".join(result)
+
+
+print(mergeAlternately("abc", "12345"))  # a1b2c3
