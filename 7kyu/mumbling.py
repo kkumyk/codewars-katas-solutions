f0efa21e18f38618939d4aa254be8979ead74fb3
kkumyk <k.kumykova@gmail.com>
2024-03-04 09:45:06 +0000

    Mumbling second solution with comments

--- 7kyu/mumbling.py
+++ 7kyu/mumbling.py
@@ -1,18 +1,41 @@
-# This time no story, no theory. The examples below show you how to write function accum:
-# accum("abcd") -> "A-Bb-Ccc-Dddd"
-# accum("cwAt") -> "C-Ww-Aaa-Tttt"
+"""
+Input > Output:
+mumbling("abcd") -> "A-Bb-Ccc-Dddd"
+mumbling("cwAt") -> "C-Ww-Aaa-Tttt"
 
+1. turn all letters to lowercase and create a letters array;
+2. iterate over the letters array and use * operator on each letter:
+- arr[i] - accesses the letter in the array
+- (i + 1) sets the number of repetition needed starting from 1
 
-def accum(st):
+"""
+
+# Solution 1
+
+def mumbling(st):
+
     newArr = []
     arr = [l.lower() for l in st]
 
-    for idx, letter in enumerate(arr):
-        subString = ""
-        subString += arr[idx].upper()
-        subString += (arr[idx] * (idx + 1))[0:-1]
-        newArr.append(subString)
-    return "-".join(newArr)
+    for i, l in enumerate(arr):
+
+        newArr.append(arr[i] * (i + 1))
+
+    return "-".join(newArr).title()
 
 
-print(accum("mumbling"))
+print(mumbling("mum"))
+
+
+# Solution 2
+
+# def mumbling(st):
+#     newArr = []
+#     arr = [l.lower() for l in st]
+
+#     for idx, letter in enumerate(arr):
+#         subString = ""
+#         subString += arr[idx].upper()
+#         subString += (arr[idx] * (idx + 1))[0:-1]
+#         newArr.append(subString)
+#     return "-".join(newArr)
